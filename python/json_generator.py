dd5e617ce556d451e8473d13c1712f6503c8c8e9
jithendrakanna <98811616+jithendrakanna@users.noreply.github.com>
2022-02-04 13:43:05 +0530

    Update json_generator.py

--- python/json_generator.py
+++ python/json_generator.py
@@ -2,34 +2,26 @@ import json
 import pandas as pd
 
 inpath = "C:/Users/jithendra.kanna/Documents/accelerator_discussion/test_file.xlsx"
-outpath = "C:/Users/jithendra.kanna/Documents/accelerator_discussion/json_control_files/"
+outpath = "C:/Users/jithendra.kanna/Documents/accelerator_discussion/json_control_files/sample/"
 
 df_excel = pd.read_excel(inpath)
 
-database = df_excel.database.unique()
+raw_table_name = df_excel.raw_table_name.unique()
 
-for i in range(len(database)):
-    # dataframe at database level
-    df_database = df_excel.loc[df_excel['database'] == database[i]]
-    raw_table_name = df_database.raw_table_name.unique()
+for i in range(len(raw_table_name)):
+    # dataframe at table level
+    df = df_excel.loc[df_excel['raw_table_name'] == raw_table_name[i]]
 
-    for y in range(len(raw_table_name)):
-        # dataframe at table level
-        df_table = df_database.loc[df_database['raw_table_name'] == raw_table_name[y]]
+    dict = {}
+    dict['application'] = ''.join([str(col) for col in df.application.unique()])
+    dict['domain'] = ''.join([str(col) for col in df.domain.unique()])
+    dict['entity_name'] = ''.join([str(col) for col in df.raw_table_name.unique()])
+    dict['configurations'] = {
+        'database': ''.join([str(col) for col in df.database.unique()]),
+        'schema': ''.join([str(col) for col in df.schema.unique()]),
+        'column_list': ','.join([str(col) for col in df.raw_column_name])
+    }
 
-        dict = {}
-        dict['application'] = df_table.application.unique().tolist()  # convert np.array to list to make it json serializable
-        dict['database'] = df_table.database.unique().tolist()
-        dict['schema'] = df_table.schema.unique().tolist()
-        dict['domain'] = df_table.domain.unique().tolist()
-        dict['raw_table_name'] = df_table.raw_table_name.unique().tolist()
-
-        column = []
-        for col in df_table.raw_column_name:
-            column.append(col)
-
-        dict['raw_column_name'] = column
-
-        j = json.dumps(dict)
-        with open(outpath + "{raw_table_name}.json".format(raw_table_name=raw_table_name[i]), 'w') as f:
-            f.write(j)
+    j = json.dumps(dict)
+    with open(outpath + "{raw_table_name}.json".format(raw_table_name=raw_table_name[i]), 'w') as f:
+        f.write(j)
